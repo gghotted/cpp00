a90b5fc18bb3ce984e9d2789ae9e19fdf74e3716
gypark <gghotted@gmail.com>
2021-05-04 17:05:23 +0900

    Update style

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,23 +1,22 @@
 #include "PhoneBook.hpp"
 
-int	main(void)
+int main(void)
 {
-	PhoneBook	phoneBook;
-	std::string	cmd;
+  PhoneBook phoneBook;
+  std::string cmd;
 
-	while (1)
-	{
-		std::cout << "cmd: ";
-
-		if (!(std::cin >> cmd))
-			phoneBook.exit("\nexit by EOF");
-		else if (cmd.compare("EXIT") == 0)
-			phoneBook.exit("bye ~");
-		else if (cmd.compare("ADD") == 0)
-			phoneBook.add();
-		else if (cmd.compare("SEARCH") == 0)
-			phoneBook.search();
-		else
-			std::cout << "invalid command! try again!\n";
-	}
+  while (1)
+  {
+    std::cout << "cmd: ";
+    if (!(std::cin >> cmd))
+      phoneBook.exit("\nexit by EOF");
+    else if (cmd.compare("EXIT") == 0)
+      phoneBook.exit("bye ~");
+    else if (cmd.compare("ADD") == 0)
+      phoneBook.add();
+    else if (cmd.compare("SEARCH") == 0)
+      phoneBook.search();
+    else
+      std::cout << "invalid command! try again!\n";
+  }
 }
